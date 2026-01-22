484c2c9f7cee53eebed39c706e9946f6fbf0bd96
Monika Choudhary <monikachoudhary2408@gmail.com>
2026-01-22 11:43:25 +0530

    changes added

--- file2.py
+++ file2.py
@@ -2,3 +2,7 @@ def print_values():
     values = [10, 20, 30, 40, 50]
     for value in values:
         print(value)
+
+
+if __name__ == "__main__":    print_values()
+    print_values()              
